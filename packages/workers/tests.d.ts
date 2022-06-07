a88d06c110bdb80c07d89e654c476527f328ffac
S Dermoumi <hi@sdrm.me>
2022-06-07 15:00:08 +0100

    Refactor type files

--- packages/workers/tests.d.ts
+++ packages/workers/tests.d.ts
@@ -1,39 +1,8 @@
+/**
+ * Workaround for conflicting Webworker types in vitest's Node scope.
+ */
+
 /// <reference no-default-lib="true"/>
-
-declare type ExtendableEvent = unknown;
-declare type Request = {
-  url: string;
-};
-declare type RequestInit = unknown;
-declare type Response = unknown;
-declare type WorkerType = unknown;
-declare type CacheQueryOptions = unknown;
-declare type ServiceWorkerRegistration = unknown;
-declare type FetchEvent = unknown;
-declare type RequestInfo = unknown;
-
-declare namespace WebAssembly {
-  type Module = unknown;
-}
-
-type ExtendableMessageEvent = {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  data: any;
-};
-
-type EventTypeMap = {
-  [key: string]: ExtendableEvent;
-  message: ExtendableMessageEvent;
-};
-
-// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
-declare interface ServiceWorkerGlobalScope {
-  addEventListener: <T extends keyof EventTypeMap>(
-    eventType: T,
-    listener: (event: EventTypeMap[T]) => void
-  ) => void;
-  skipWaiting: () => void;
-}
 
 // eslint-disable-next-line @typescript-eslint/consistent-type-definitions, unicorn/prevent-abbreviations
 declare interface ImportMetaEnv {
