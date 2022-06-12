0c6800c3f8f93c499783a927a76d100dfd1d3492
S Dermoumi <hi@sdrm.me>
2022-06-12 22:21:09 +0100

    Add input manager

---
+++ packages/client/src/utils/input-manager/input-manager.spec.ts
@@ -0,0 +1,242 @@
+import InputManager, { InputAction, InputAxis } from "./input-manager";
+import userEvent from "@testing-library/user-event";
+import type { UserEvent } from "@testing-library/user-event/dist/types/setup";
+import { fireEvent } from "@testing-library/react";
+
+describe("InputManager", () => {
+  const keymap = {
+    ArrowUp: InputAction.Up,
+    ArrowRight: InputAction.Right,
+    ArrowDown: InputAction.Down,
+    ArrowLeft: InputAction.Left,
+    KeyF: InputAction.Interact,
+  };
+
+  beforeEach((context) => {
+    // Create an input manager
+    const input = new InputManager();
+    input.setKeymap(keymap);
+    input.bind();
+
+    context.input = input;
+
+    // Create an input user
+    const user = userEvent.setup({
+      skipAutoClose: true,
+    });
+    context.user = user;
+  });
+
+  afterEach((context) => {
+    const input = context.input as InputManager;
+    input.dispose();
+  });
+
+  it("tracks down keys", async (context) => {
+    // Given an input manager
+    // When I press a key
+    // Then its action should be marked as pressed
+    // When I release that same key
+    // Then its action should be marked as not pressed
+
+    const input = context.input as InputManager;
+    const user = context.user as UserEvent;
+
+    await user.keyboard("[KeyF>]");
+
+    expect(input.isActionDown(InputAction.Interact)).toBe(true);
+
+    await user.keyboard("[/KeyF]");
+
+    expect(input.isActionDown(InputAction.Interact)).toBe(false);
+  });
+
+  it("ignores repeated down keys", async (context) => {
+    // Given an input manager
+    // When I press a key twice
+    // Then its action should be marked as pressed
+    // When I release that same key
+    // Then it should be marked as not pressed
+
+    const input = context.input as InputManager;
+    const user = context.user as UserEvent;
+
+    await user.keyboard("[KeyF>][KeyF>]");
+
+    expect(input.isActionDown(InputAction.Interact)).toBe(true);
+
+    await user.keyboard("[/KeyF]");
+
+    expect(input.isActionDown(InputAction.Interact)).toBe(false);
+  });
+
+  it("ignores keys not in the key map", async (context) => {
+    // Given an input manager
+    // When I press a non-mapped key
+    // Then all actions should be marked as non pressed
+
+    const input = context.input as InputManager;
+    const user = context.user as UserEvent;
+
+    await user.keyboard("[KeyN]");
+
+    for (const action of Object.values(InputAction)) {
+      expect(input.isActionDown(action)).toBe(false);
+    }
+  });
+
+  it("tracks pressed keys", async (context) => {
+    // Given an input manager
+    // When I press a key
+    // Then the key's action should be marked as just pressed
+
+    const input = context.input as InputManager;
+    const user = context.user as UserEvent;
+
+    await user.keyboard("[KeyF>]");
+
+    expect(input.isActionPressed(InputAction.Interact)).toBe(true);
+  });
+
+  it("tracks released keys", async (context) => {
+    // Given an input manager
+    // When I press a key
+    // And wait for the next update
+    // And I release that key
+    // Then the key's action should be marked as just released
+
+    const input = context.input as InputManager;
+    const user = context.user as UserEvent;
+
+    await user.keyboard("[ArrowUp>]");
+    input.update();
+
+    await user.keyboard("[/ArrowUp]");
+
+    expect(input.isActionReleased(InputAction.Up)).toBe(true);
+  });
+
+  it("maps direction actions to axes", async (context) => {
+    // Given an input manager
+    // When I press the left arrow key
+    // Then the left X axis should be at -1
+
+    const input = context.input as InputManager;
+    const user = context.user as UserEvent;
+
+    expect(input.getAxis(InputAxis.LX)).not.toBe(-1);
+
+    await user.keyboard("[ArrowLeft>]");
+
+    expect(input.getAxis(InputAxis.LX)).toBe(-1);
+  });
+
+  it("respects direction actions order when mapping to axes", async (context) => {
+    // Given an input manager
+    // When I press the up arrow key
+    // Then I press the down arrow key 3 times
+    // Then I release the up arrow key
+    // Then the left Y axis should be at -1
+
+    const input = context.input as InputManager;
+    const user = context.user as UserEvent;
+
+    expect(input.getAxis(InputAxis.LY)).not.toBe(-1);
+
+    await user.keyboard("[ArrowUp>][ArrowDown>3/]");
+
+    expect(input.getAxis(InputAxis.LY)).toBe(-1);
+  });
+
+  it("clears down keys when the window blurs", async (context) => {
+    // Given an input manager
+    // When I press a key
+    // Then blur the window
+    // Then the key's action should be marked as not pressed
+
+    const input = context.input as InputManager;
+    const user = context.user as UserEvent;
+
+    await user.keyboard("[KeyF>]");
+    expect(input.isActionDown(InputAction.Interact)).toBe(true);
+
+    fireEvent.blur(window);
+
+    expect(input.isActionDown(InputAction.Interact)).toBe(false);
+  });
+
+  it("resets axes when the window blurs", async (context) => {
+    // Given an input manager
+    // When I press the down arrow key
+    // And I blur the window
+    // Then the left Y axis should be at 0
+
+    const input = context.input as InputManager;
+    const user = context.user as UserEvent;
+
+    await user.keyboard("[ArrowDown>]");
+    expect(input.getAxis(InputAxis.LY)).toBe(1);
+
+    fireEvent.blur(window);
+
+    expect(input.getAxis(InputAxis.LY)).toBe(0);
+  });
+
+  it("resets axes' action stacks when the window blurs", async (context) => {
+    // Given an input manager
+    // When I press the left arrow key
+    // And I blur the window
+    // And I press and release the right arrow key
+    // Then the left X axis should be at 0
+
+    const input = context.input as InputManager;
+    const user = context.user as UserEvent;
+
+    await user.keyboard("[ArrowLeft>]");
+    expect(input.getAxis(InputAxis.LX)).toBe(-1);
+
+    fireEvent.blur(window);
+
+    await user.keyboard("[ArrowRight]");
+    expect(input.getAxis(InputAxis.LX)).toBe(0);
+  });
+
+  it("does nothing when a direction key that's not in the axis is released", (context) => {
+    // Given an input manager
+    // Adn a key that's already pressed (outside the window)
+    // When I release the key
+    // Then the axis value should not change
+
+    const input = context.input as InputManager;
+
+    expect(input.getAxis(InputAxis.LY)).toBe(0);
+
+    fireEvent.keyUp(window, { code: "ArrowUp" });
+    expect(input.getAxis(InputAxis.LY)).toBe(0);
+  });
+
+  it("ignores input when an input element is focused", async (context) => {
+    // Given an input manager
+    // And an input element that's focused
+    // When I press a key
+    // Then the key's action should not be marked as pressed
+
+    const input = context.input as InputManager;
+    const user = context.user as UserEvent;
+
+    const inputElement = document.createElement("input");
+    document.body.append(inputElement);
+
+    await user.click(inputElement);
+
+    await user.keyboard("[KeyF>]");
+    expect(input.isActionDown(InputAction.Interact)).toBe(false);
+    expect(input.isActionPressed(InputAction.Interact)).toBe(false);
+
+    input.update();
+    await user.keyboard("[/KeyF]");
+    expect(input.isActionReleased(InputAction.Interact)).toBe(false);
+
+    expect(inputElement.value).toBe("f");
+  });
+});
---
+++ packages/client/src/utils/input-manager/input-manager.ts
@@ -0,0 +1,247 @@
+import { ArrayMap, SetMap } from "../default-map";
+
+export enum InputAction {
+  Interact = "interact",
+  Cancel = "cancel",
+  Up = "up",
+  Right = "right",
+  Down = "down",
+  Left = "left",
+}
+export type DirectionInputAction =
+  | InputAction.Down
+  | InputAction.Left
+  | InputAction.Right
+  | InputAction.Up;
+
+export enum InputAxis {
+  LX = "lx",
+  LY = "ly",
+  RX = "rx",
+  RY = "ry",
+  LT = "lt",
+  RT = "rt",
+}
+
+export type KeyboardMapping = Record<string, InputAction>;
+
+const DIRECTION_TO_AXIS = {
+  [InputAction.Up]: -1,
+  [InputAction.Down]: 1,
+  [InputAction.Left]: -1,
+  [InputAction.Right]: 1,
+} as const;
+
+type AxesValues = Record<InputAxis, number>;
+
+/**
+ * Manages input for a single context.
+ */
+export default class InputManager {
+  private readonly keysDownPerAction = new SetMap<InputAction, string>();
+  private readonly currentKeysDown = new Set<InputAction>();
+  private previousKeysDown = new Set<InputAction>();
+  private keymap: KeyboardMapping = {};
+
+  private readonly axesActionStacks = new ArrayMap<
+    InputAxis,
+    DirectionInputAction
+  >();
+
+  private readonly axesValues: AxesValues = {
+    [InputAxis.LX]: 0,
+    [InputAxis.LY]: 0,
+    [InputAxis.LT]: 0,
+    [InputAxis.RX]: 0,
+    [InputAxis.RY]: 0,
+    [InputAxis.RT]: 0,
+  };
+
+  public constructor() {
+    this.update = this.update.bind(this);
+    this.keydownHandler = this.keydownHandler.bind(this);
+    this.keyupHandler = this.keyupHandler.bind(this);
+    this.blurHandler = this.blurHandler.bind(this);
+  }
+
+  public setKeymap(keymap: KeyboardMapping): void {
+    this.keymap = keymap;
+  }
+
+  public bind(): void {
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    window.addEventListener("keydown", this.keydownHandler);
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    window.addEventListener("keyup", this.keyupHandler);
+
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    window.addEventListener("blur", this.blurHandler);
+  }
+
+  public dispose(): void {
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    window.removeEventListener("blur", this.blurHandler);
+
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    window.removeEventListener("keyup", this.keyupHandler);
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    window.removeEventListener("keydown", this.keydownHandler);
+  }
+
+  public isActionDown(action: InputAction): boolean {
+    return this.currentKeysDown.has(action);
+  }
+
+  public isActionPressed(action: InputAction): boolean {
+    return (
+      this.currentKeysDown.has(action) && !this.previousKeysDown.has(action)
+    );
+  }
+
+  public isActionReleased(action: InputAction): boolean {
+    return (
+      !this.currentKeysDown.has(action) && this.previousKeysDown.has(action)
+    );
+  }
+
+  public getAxis(axis: keyof AxesValues): number {
+    return this.axesValues[axis];
+  }
+
+  public update(): void {
+    this.previousKeysDown = new Set(this.currentKeysDown);
+  }
+
+  private keydownHandler(event: KeyboardEvent): void {
+    // Ignore this if an input is currently focused
+    if (this.isInputElementActive()) return;
+
+    const { code } = event;
+
+    // Check if this key is mapped to an action
+    const action = this.keymap[code];
+    if (action === undefined) return;
+
+    // Prevent default behavior
+    event.preventDefault();
+    event.stopPropagation();
+
+    // Mark the key as currently down
+    const actionKeysDown = this.keysDownPerAction.get(action);
+    const repeat = actionKeysDown.has(code);
+
+    if (!repeat) {
+      actionKeysDown.add(code);
+      this.currentKeysDown.add(action);
+
+      // Trigger axis update
+      switch (action) {
+        case InputAction.Up:
+        case InputAction.Right:
+        case InputAction.Down:
+        case InputAction.Left:
+          this.axesActionDown(action);
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
+  private keyupHandler(event: KeyboardEvent): void {
+    // Ignore this if an input is currently focused
+    if (this.isInputElementActive()) return;
+
+    const { code } = event;
+
+    // Check if this key is mapped to an action
+    const action = this.keymap[code];
+    if (action === undefined) return;
+
+    // Prevent default behavior
+    event.preventDefault();
+    event.stopPropagation();
+
+    // Mark the key as currently up
+    const actionKeysDown = this.keysDownPerAction.get(action);
+    actionKeysDown.delete(code);
+    if (actionKeysDown.size === 0) {
+      this.currentKeysDown.delete(action);
+    }
+
+    // Trigger axis update
+    switch (action) {
+      case InputAction.Up:
+      case InputAction.Right:
+      case InputAction.Down:
+      case InputAction.Left:
+        this.axesActionUp(action);
+        break;
+      default:
+        break;
+    }
+  }
+
+  private blurHandler(): void {
+    // Reset actions that are "down"
+    this.keysDownPerAction.clear();
+    this.currentKeysDown.clear();
+
+    // Reset axes
+    this.axesActionStacks.clear();
+    for (const axis of Object.keys(this.axesValues)) {
+      this.axesValues[axis as InputAxis] = 0;
+    }
+  }
+
+  private axesActionDown(action: DirectionInputAction): void {
+    const axis =
+      action === InputAction.Up || action === InputAction.Down
+        ? InputAxis.LY
+        : InputAxis.LX;
+    const stack = this.axesActionStacks.get(axis);
+
+    // Only change axis value if this action was not the last one in the stack
+    const lastDirection = stack[0];
+    if (action !== lastDirection) {
+      const value = DIRECTION_TO_AXIS[action];
+
+      this.axesValues[axis] = value;
+    }
+
+    // Add the action to the stack
+    stack.unshift(action);
+  }
+
+  private axesActionUp(action: DirectionInputAction): void {
+    const axis =
+      action === InputAction.Up || action === InputAction.Down
+        ? InputAxis.LY
+        : InputAxis.LX;
+    const stack = this.axesActionStacks.get(axis);
+
+    // If the action is not in the stack, then there's nothing to do
+    const actionIndex = stack.indexOf(action);
+    if (actionIndex === -1) return;
+
+    // Remove the action from the stack
+    stack.splice(actionIndex, 1);
+
+    // If the removed action was the first in the stack, then update the axis
+    // value to the previous action. Or to 0 if there are no previous actions.
+    if (actionIndex === 0) {
+      const lastDirection = stack[0];
+      const value =
+        lastDirection === undefined ? 0 : DIRECTION_TO_AXIS[lastDirection];
+
+      this.axesValues[axis] = value;
+    }
+  }
+
+  private isInputElementActive(): boolean {
+    const focusedElement = document.activeElement;
+    if (!focusedElement) return false;
+
+    return ["INPUT", "TEXTAREA"].includes(focusedElement.tagName);
+  }
+}
