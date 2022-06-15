1fe8e75ca7921fabdc06cd6a2984d1aca97fcb35
S Dermoumi <hi@sdrm.me>
2022-06-15 23:05:00 +0100

    Fix version for iOS

--- packages/client/cordova/hooks/update-version.js
+++ packages/client/cordova/hooks/update-version.js
@@ -1,8 +1,9 @@
 /* eslint-disable unicorn/prefer-module */
 
 const path = require("node:path");
-const fs = require("node:fs");
-const xmlToJs = require("xml2js");
+const fs = require("node:fs/promises");
+const glob = require("glob");
+const libxml = require("libxmljs");
 
 const SUFFIX_TO_CODE = {
   alpha: 10,
@@ -13,9 +14,12 @@ const SUFFIX_TO_CODE = {
   rc2: 32,
 };
 
-function calculateVersionCode(fullVersion) {
+function getVersionCode(fullVersion) {
   const [version, suffix] = fullVersion.split("-");
-  const suffixCode = SUFFIX_TO_CODE[suffix] || 50;
+  const suffixCode = suffix === undefined ? 50 : SUFFIX_TO_CODE[suffix];
+  if (suffixCode === undefined) {
+    throw new Error(`ERROR: Unknown version suffix "${fullVersion}"`);
+  }
 
   const [major, minor, patch] = version
     .split(".")
@@ -24,27 +28,89 @@ function calculateVersionCode(fullVersion) {
   return major * 10_000_000 + minor * 100_000 + patch * 100 + suffixCode;
 }
 
+function getTrimmedVersion(fullVersion) {
+  const [version] = fullVersion.split("-");
+
+  const [major, minor, patch] = version
+    .split(".")
+    .map((v) => Number.parseInt(v, 10));
+
+  return `${major}.${minor}.${patch}`;
+}
+
+async function updateAndroidManifest(projectRoot, version) {
+  const manifestPath = path.join(
+    projectRoot,
+    "platforms/android/app/src/main/AndroidManifest.xml"
+  );
+  const manifestXml = await fs.readFile(manifestPath, "utf8");
+  const manifestDocument = libxml.parseXml(manifestXml);
+
+  manifestDocument
+    .root()
+    .attr("android:versionName", version)
+    .attr("android:versionCode", getVersionCode(version));
+
+  return fs.writeFile(manifestPath, manifestDocument.toString());
+}
+
+async function updateIOSConfig(projectRoot, version) {
+  const configXmlMatches = glob.sync(
+    path.join(projectRoot, "platforms/ios/*/config.xml")
+  );
+
+  if (configXmlMatches.length === 0) {
+    throw new Error("ERROR: Could not find IOS config.xml");
+  }
+
+  const configPath = configXmlMatches[0];
+  const configXml = await fs.readFile(configPath, "utf8");
+  const configDocument = libxml.parseXml(configXml);
+
+  configDocument.root().attr("version", version);
+
+  return fs.writeFile(configPath, configDocument.toString());
+}
+
+async function updateIOSPlist(projectRoot, version) {
+  const plistMatches = glob.sync(
+    path.join(projectRoot, "platforms/ios/*/*-Info.plist")
+  );
+
+  if (plistMatches.length === 0) {
+    throw new Error("ERROR: Could not find IOS Info.plist");
+  }
+
+  const plistPath = plistMatches[0];
+  const plistXml = await fs.readFile(plistPath, "utf8");
+  const plistDocument = libxml.parseXmlString(plistXml);
+
+  plistDocument
+    .get("//key[.='CFBundleShortVersionString']/following-sibling::string")
+    .text(version);
+
+  plistDocument
+    .get("//key[.='CFBundleVersion']/following-sibling::string")
+    .text(getTrimmedVersion(version));
+
+  return fs.writeFile(plistPath, plistDocument.toString());
+}
+
 module.exports = async (context) => {
   const { projectRoot, platforms } = context.opts;
 
-  const { version } = require(path.join(projectRoot, `package.json`));
-  const versionCode = calculateVersionCode(version);
+  const { version } = require(path.join(projectRoot, "package.json"));
 
   const pending = platforms.map(async (platform) => {
     if (platform === "android") {
-      const manifestPath = path.join(
-        "platforms/android/app/src/main/AndroidManifest.xml"
-      );
-      const manifestXml = fs.readFileSync(manifestPath, "utf8");
-      const manifestJson = await xmlToJs.parseStringPromise(manifestXml);
+      return updateAndroidManifest(projectRoot, version);
+    }
 
-      const manifestNode = manifestJson.manifest;
-      manifestNode.$["android:versionName"] = version;
-      manifestNode.$["android:versionCode"] = versionCode;
-
-      const manifestBuilder = new xmlToJs.Builder();
-      const manifestXmlNew = manifestBuilder.buildObject(manifestJson);
-      fs.writeFileSync(manifestPath, manifestXmlNew);
+    if (platform === "ios") {
+      return Promise.all([
+        updateIOSConfig(projectRoot, version),
+        updateIOSPlist(projectRoot, version),
+      ]);
     }
   });
 
