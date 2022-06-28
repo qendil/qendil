9fab112a59544f52710954353d99e8a0d6cb7b1d
S Dermoumi <hi@sdrm.me>
2022-06-28 02:27:14 +0100

    Fix GameSystem not being exported as a class

--- packages/client/src/utils/game-world/index.ts
+++ packages/client/src/utils/game-world/index.ts
@@ -1,7 +1,8 @@
 export { default } from "./game-world";
 export { default as GameComponent } from "./game-component";
+export { default as GameSystem } from "./game-system";
 
 export type { GameComponentFilter } from "./game-component";
 export type { EntityQuery } from "./entity-query";
-export type { default as GameSystem, GameSystemHandle } from "./game-system";
+export type { GameSystemHandle } from "./game-system";
 export type { GameEntity } from "./game-entity";
