b55132e1e8ecbe59b2303ac27d62c0b61154a690
Dermoumi S <hi@sdrm.me>
2022-06-29 21:22:27 +0100

    Fix useWasm retrying indefinitely when the initializer resolves to undefined (#129)

--- packages/client/src/hooks/use-wasm/use-wasm.spec.tsx
+++ packages/client/src/hooks/use-wasm/use-wasm.spec.tsx
@@ -10,10 +10,9 @@ describe("useWasm hook", () => {
     // When I wait for the wasm initializer to resolve
     // Then the component should render correctly
 
-    // eslint-disable-next-line unicorn/no-useless-promise-resolve-reject
-    const dummyInitializer = vi.fn(async () => Promise.resolve(42));
+    // eslint-disable-next-line @typescript-eslint/require-await
+    const dummyInitializer = vi.fn(async () => 42);
 
-    // When I use the useAsset hook
     const { result } = renderHook(() => useWasm(dummyInitializer), {
       wrapper: ({ children }) => <Suspense>{children}</Suspense>,
     });
@@ -22,6 +21,29 @@ describe("useWasm hook", () => {
 
     await act(async () => new Promise(process.nextTick));
 
-    expect(result.current).toBeDefined();
+    expect(result.current).toBe(42);
+  });
+
+  it("works even if the initializer returns undefined", async () => {
+    // Given a component that uses useWasm
+    // And a wasm initializer that resolves to undefined
+    // When I try to render the component
+    // Then the suspense fallback should render instead
+    // When I wait for the wasm initializer to resolve
+    // Then the component should render correctly
+
+    const dummyInitializer = vi.fn(async () => {
+      // Resolves to undefined
+    });
+
+    const { result } = renderHook(() => useWasm(dummyInitializer), {
+      wrapper: ({ children }) => <Suspense>{children}</Suspense>,
+    });
+
+    expect(result.current).toBeNull();
+
+    await act(async () => new Promise(process.nextTick));
+
+    expect(result.current).toBe(undefined);
   });
 });
